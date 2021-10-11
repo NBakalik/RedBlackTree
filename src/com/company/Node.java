5c7c0bcc024182737c98b643eec5556e45a3f34c
PbhkoGOE <MYub3HX06A>
2021-10-11 13:31:20 +0300

    added Node class

---
+++ src/com/company/Node.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Node {
+    int data;
+
+    Node left;
+    Node right;
+    Node parent;
+
+    Color color;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
